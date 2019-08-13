871c8fae9d2aadd96783a33f6958446c5aa21f54
Bradley Dice <bdice@bradleydice.com>
2019-08-13 11:46:36 -0400

    Correct swapped test files.

--- molssiexample/tests/test_molssiexample.py
+++ molssiexample/tests/test_molssiexample.py
@@ -5,6 +5,8 @@ Unit and regression test for the molssiexample package.
 # Import package, test suite, and other packages as needed
 import molssiexample
 import pytest
+import sys
 
-def test_euler():
-    assert molssiexample.euler(2) == 2
+def test_molssiexample_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "molssiexample" in sys.modules
