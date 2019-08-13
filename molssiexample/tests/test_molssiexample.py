cb4de37e575911bf87ad78d8d27e3f0335480e52
Bradley Dice <bdice@bradleydice.com>
2019-08-13 11:32:32 -0400

    Move euler test.

--- molssiexample/tests/test_molssiexample.py
+++ molssiexample/tests/test_molssiexample.py
@@ -5,11 +5,6 @@ Unit and regression test for the molssiexample package.
 # Import package, test suite, and other packages as needed
 import molssiexample
 import pytest
-import sys
-
-def test_molssiexample_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "molssiexample" in sys.modules
 
 def test_euler():
     assert molssiexample.euler(2) == 2
