95072c51763dfc8a83b492bff800dbf9cda42421
Bradley Dice <bdice@bradleydice.com>
2019-08-13 14:24:11 -0400

    Add docstrings, update function signatures.

--- molssiexample/math.py
+++ molssiexample/math.py
@@ -1,9 +1,37 @@
 import numpy as np
 
-def euler(n: int = 10, x: float = 1):
+def euler(n: int):
+    """
+    Compute an approximation of e, Euler's number, using Taylor series.
+
+    Parameters
+    ----------
+    n : int
+        Number of terms to use in Taylor series approximation.
+
+    Returns
+    -------
+    e_approx : float
+        Approximation of e.
+    """
     if n < 0:
         raise ValueError('Must use a non-negative value for n.')
-    return np.sum([x**i/np.math.factorial(i) for i in range(n+1)])
+    e_approx = np.sum([1/np.math.factorial(i) for i in range(n+1)])
+    return e_approx
 
-def pi(n: int = 100):
-    return 4 * np.mean(np.linalg.norm(np.random.rand(n, 2), axis=-1) < 1)
+def pi(n: int):
+    """
+    Compute an approximation of pi using Monte Carlo sampling.
+
+    Parameters
+    ----------
+    n : int
+        Number of Monte Carlo samples.
+
+    Returns
+    -------
+    pi_approx : float
+        Approximation of pi.
+    """
+    pi_approx = 4 * np.mean(np.linalg.norm(np.random.rand(n, 2), axis=-1) < 1)
+    return pi_approx
