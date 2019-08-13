2b5b901efa628d78a0b7d336baff52f41e9dccb0
Bradley Dice <bdice@bradleydice.com>
2019-08-13 11:52:34 -0400

    Fix number of terms, add test.

--- molssiexample/euler.py
+++ molssiexample/euler.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def euler(n: int = 10, x: float = 1):
-    return np.sum([x**i/np.math.factorial(i) for i in range(n)])
+    return np.sum([x**i/np.math.factorial(i) for i in range(n+1)])
--- molssiexample/tests/test_euler.py
+++ molssiexample/tests/test_euler.py
@@ -6,5 +6,9 @@ Unit and regression test for the molssiexample package.
 import molssiexample
 import pytest
 
-def test_euler():
-    assert molssiexample.euler(2) == 2
+@pytest.mark.parametrize(['n', 'answer'], [(0, 1), (1, 2), (2, 2.5), (3, 2.6666667)])
+def test_euler(n, answer):
+    assert molssiexample.euler(n) == pytest.approx(answer, abs=1e-6)
+
+if __name__ == '__main__':
+    pytest.main()
